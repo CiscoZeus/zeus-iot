6400eb5f63c62bb2436aa8de575d55fce1dea8ff
yindolia <yindolia@gmail.com>
2016-05-05 15:57:38 +0530

    Update APIsendMetric.py

--- programs/APIsendMetric.py
+++ programs/APIsendMetric.py
@@ -10,6 +10,7 @@ from zeus import client as zc
 z= zc.ZeusClient('f0069791', 'api.ciscozeus.io')
 
 def Metricsend(a,name,ts):
+    #a is value to be transferred, name is name of metric and ts is the timestamp
     metric = [{"timestamp":ts, "point":{"value":a}}]
     z.sendMetric(name,metric)
     print "."
