812335036973a342248346e99e2186441b450382
yindolia <yindolia@gmail.com>
2016-05-26 12:13:35 +0530

    Update PIR_presence_Sensor.py

--- programs/PIR_presence_Sensor.py
+++ programs/PIR_presence_Sensor.py
@@ -1,3 +1,6 @@
+#This is an example of sensing and actuation. 
+#We will make a LED glow when the PIR sensor detects motion.
+
 import RPi.GPIO as gpio
 import time
 import json
@@ -9,6 +12,7 @@ gpio.setup(3,gpio.OUT)
 gpio.setup(11,gpio.IN)
 
 
+
 while True:
 	
 	i= gpio.input(11)
@@ -16,16 +20,16 @@ while True:
 		print "No one",i
 		gpio.output(3,0)
 		time.sleep(1)
-		payload= {"json": json.dumps({"PIR":"no one", "LED":"off"})}
+	#	payload= {"json": json.dumps({"PIR":"no one", "LED":"off"})}
 		
 
 	elif i==1:
 		print "Someone",i
 		gpio.output(3,1)
 		time.sleep(1)
-		payload= {"json": json.dumps({"PIR":"Someone", "LED":"on"})}
+	#	payload= {"json": json.dumps({"PIR":"Someone", "LED":"on"})}
 	
-	url = 'http://127.0.0.1:8888/collect.PIR'+str(i)
+	'''url = 'http://127.0.0.1:8888/collect.PIR'+str(i)
 	r = requests.post(url, data = payload)
-	print('sent')
+	print('sent')'''
 
